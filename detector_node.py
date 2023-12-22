9b2378293b776ff7b2b2f21f5e44468be361f3ae
leslieecv <leslie.cardenas008@gmail.com>
2023-12-22 17:43:24 -0300

    working nodes on maqui

--- detector_node.py
+++ detector_node.py
@@ -3,21 +3,21 @@
 import rospy
 import time
 import hardcoded_bridge as wtf
-from detector import FaceDetector
-from std_msgs.msg import Image
-from msg.ImageArray import ImageArray
+from sensor_msgs.msg import Image
+from uchile_msgs.msg import ImageArray
+from detector.detect import FaceDetector
 
 class face_detector_node():
     def __init__(self):
+        print('Nodo creado')
         self.subscriber = rospy.Subscriber('/maqui/camera/front/image_raw', Image, self._callback)
-        self.publisher = rospy.Publisher('/maqui/interactions/face_detection', ImageArray, queue_size=10, latch=True)
+        self.publisher = rospy.Publisher('/maqui/interactions/face_detection', ImageArray, queue_size=5, latch=True)
         self.face_detector = FaceDetector(keep_top_k = 3)
 
     def _callback(self, data):
-        
         cv_image = wtf.imgmsg_to_cv2(data)
         self.face_detector.detect(cv_image)
-        self.face_detector.cut_faces(save_face = True)
+        self.face_detector.cut_faces()
 
         output = ImageArray()
         output.header.stamp = rospy.Time.now()
@@ -25,9 +25,8 @@ class face_detector_node():
             for face in self.face_detector.faces:
                 imgmsg = wtf.cv2_to_imgmsg(face)
                 output.data.append(imgmsg)
-            print(f'Sending face image')
+                print(f'Sending face image')
             self.publisher.publish(output)
-            time.sleep(5)
         except:
             with Exception as e:
                 print(e)
--- recognition_node.py
+++ recognition_node.py
@@ -4,24 +4,22 @@ import rospy
 import time
 import hardcoded_bridge as wtf
 from std_msgs.msg import String
-from msg.ImageArray import ImageArray
-from msg.StringArray import StringArray
-
-from sensor_msgs.msg import Image
+from uchile_msgs.msg import ImageArray, StringArray
 from recognizer.config import get_config
 from recognizer.recognize import FaceRecognizer
-from PIL import Image
 
 class face_recognition_node():
     def __init__(self):
-        self.subscriber = rospy.Subscriber('/maqui/interactions/face_detection', ImageArray, self._callback)
-        self.publisher = rospy.Publisher('/maqui/interactions/face_recognition', Image, queue_size=10, latch=True)
-        
         conf = get_config(training = False, mobile = True)
         self.face_recognizer = FaceRecognizer(conf)
 
+        self.subscriber = rospy.Subscriber('/maqui/interactions/face_detection', ImageArray, self._callback)
+        self.publisher = rospy.Publisher('/maqui/interactions/face_recognition', StringArray, queue_size=5, latch=True)
+
     def _callback(self, data):
-        cv_images = [wtf.imgmsg_to_cv2(image_msg) for image_msg in data.image_list]
+        #print('data:', data)
+        cv_images = [wtf.imgmsg_to_cv2(image_msg) for image_msg in data.data]
+        #print('imgs:',cv_images)
         names = self.face_recognizer.recognize_faces(cv_images)
         output = StringArray()
         output.header.stamp = rospy.Time.now()
@@ -32,7 +30,6 @@ class face_recognition_node():
             
             print(f'Sending name of recognized person')
             self.publisher.publish(output)
-            time.sleep(5)
         except:
             with Exception as e:
                 print(e)
